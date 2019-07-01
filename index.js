ef3f985414654c860618f0f7509ee43a228eb50f
Clark Chen <yyunqingchenn@gmail.com>
2019-07-01 01:32:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name)
 {
-  return kittens.push(name)
+  return kittens.push(name);
+}
+
+function destructivelyPrependKitten(name)
+{
+  return kittens.unshift(name);
 }
 
 function 
